e7d20943736a18bba66d961d916b5fc40d742720
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-04-27 22:23:50 -0500

    added surf features to logistic regression classifier

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -1,12 +1,15 @@
-import mahotas as mh
-import numpy as np
 from glob import glob
 from jug import TaskGenerator
 import pandas as pd
+import mahotas as mh
+from mahotas.features import surf
+import numpy as np
+from sklearn import cross_validation
+from sklearn.cluster import KMeans
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn import cross_validation
+
 
 def texture(im):
     '''Compute features for an image
@@ -71,12 +74,14 @@ def compute_lbp(fname):
 def accuracy(featureType, features, labels, predict=False, test_features=[], test_images=[]):
     # We use logistic regression because it is very fast.
     # Feel free to experiment with other classifiers
-    clf = Pipeline([('preproc', StandardScaler()),
-                ('classifier', LogisticRegression(solver="lbfgs", multi_class="multinomial"))])
+    clf = Pipeline([
+                    ('preproc', StandardScaler()),
+                    ('classifier', LogisticRegression(solver="lbfgs", multi_class="multinomial"))
+                    ])
     clf.fit(features, labels)
 
     if predict:
-        print "Predicting test images"
+        print("Predicting test images")
         preds =  clf.predict_proba(test_features)
         create_submission(featureType, preds, test_images)
 
@@ -84,6 +89,14 @@ def accuracy(featureType, features, labels, predict=False, test_features=[], tes
     scores = cross_validation.cross_val_score(
         clf, features, labels, cv=cv)
     return scores.mean()
+
+@TaskGenerator
+def print_results(scores):
+    with open('submissions/results.image.txt', 'w') as output:
+        for k,v in scores:
+            output.write('Accuracy with Logistic Regression [{0}]: {1:.1%}\n'.format(
+                k, v.mean()))
+
 
 def create_submission(featureType, preds, images):
     print('Creating submission')
@@ -106,24 +119,17 @@ def create_submission(featureType, preds, images):
     submission_data[['img', 'c0', 'c1', 'c2', 'c3',
          'c4', 'c5', 'c6', 'c7', 'c8', 'c9']].to_csv(fileName, index=False)
 
-@TaskGenerator
-def print_results(scores):
-    with open('submissions/results.image.txt', 'w') as output:
-        for k,v in scores:
-            output.write('Accuracy (LOO x-val) with Logistic Regression [{0}]: {1:.1%}\n'.format(
-                k, v.mean()))
-
 def get_images(train):
     classes = ['c0','c1','c2','c3','c4','c5','c6','c7','c8','c9']
     images = []
     # Use glob to get all the train_images
     if train:
         for i in classes:
-            images += glob('{}/{}/{}/*.jpg'.format('imgs', 'train', i))[:5]
+            images += glob('{}/{}/{}/*.jpg'.format('imgs', 'train', i))[:20]
 
     # Use glob to get all the test_images
     else:
-        images += glob('{}/{}/*.jpg'.format('imgs', 'test'))[:5]
+        images += glob('{}/{}/*.jpg'.format('imgs', 'test'))[:20]
 
     images.sort()
     return images
@@ -133,6 +139,7 @@ def get_features(train, images):
     chists = []
     lbps = []
     labels = []
+    alldescriptors = []
     for fname in images:
         haralicks.append(compute_texture(fname))
         chists.append(compute_chist(fname))
@@ -140,13 +147,33 @@ def get_features(train, images):
         if train:
             label = fname.split('/')[2]
             labels.append(label)
+        
+        im = mh.imresize(mh.imread(fname, as_grey=True), (300,200))
+        im = im.astype(np.uint8)
+        # To use dense sampling, you can try the following line:
+        alldescriptors.append(surf.dense(im, spacing=16))
+        #alldescriptors.append(surf.surf(im, descriptor_only=True))
+    k = 5 #32 #256
+    km = KMeans(k)
+
+    concatenated = np.concatenate(alldescriptors)
+    print('Number of descriptors: {}'.format(
+            len(concatenated)))
+    concatenated = concatenated[::64]
+    print('Clustering with K-means...')
+    km.fit(concatenated)
+    surf_descriptors = []
+    for d in alldescriptors:
+        c = km.predict(d)
+        surf_descriptors.append(np.bincount(c, minlength=k))
     
+    surf_descriptors = to_array(surf_descriptors, dtype=float)
     haralicks = to_array(haralicks)
     chists = to_array(chists)
     lbps = to_array(lbps)
     labels = to_array(labels)
 
-    return haralicks, chists, lbps, labels
+    return haralicks, chists, lbps, labels, surf_descriptors
 
 
 train_images = get_images(True)
@@ -155,17 +182,18 @@ test_images = get_images(False)
 to_array = TaskGenerator(np.array)
 hstack = TaskGenerator(np.hstack)
 
-haralicks, chists, lbps, labels = get_features(True, train_images)
+haralicks, chists, lbps, labels, surf_descriptors = get_features(True, train_images)
 combined = hstack([chists, haralicks])
-combined_all = hstack([chists, haralicks, lbps])
+combined_all = hstack([chists, haralicks, lbps, surf_descriptors])
 
-test_haralicks, test_chists, test_lbps, test_labels = get_features(False, test_images)
+test_haralicks, test_chists, test_lbps, test_labels, test_surf = get_features(False, test_images)
 test_combined = hstack([test_chists, test_haralicks])
-test_combined_all = hstack([test_chists, test_haralicks, test_lbps])
+test_combined_all = hstack([test_chists, test_haralicks, test_lbps, test_surf])
 
 scores_base = accuracy('base', haralicks, labels, True, test_haralicks, test_images)
 scores_chist = accuracy('chists', chists, labels, True, test_chists, test_images)
 scores_lbps = accuracy('lbps', lbps, labels, True, test_lbps, test_images)
+scores_surf = accuracy('surf', surf_descriptors, labels, True, test_surf, test_images)
 scores_combined = accuracy('combined', combined, labels, True, test_combined, test_images)
 scores_combined_all = accuracy('combined_all', combined_all, labels, True, test_combined_all, test_images)
 
@@ -173,6 +201,7 @@ print_results([
         ('base', scores_base),
         ('chists', scores_chist),
         ('lbps', scores_lbps),
+        ('surf', scores_surf),
         ('combined' , scores_combined),
         ('combined_all' , scores_combined_all),
         ])
